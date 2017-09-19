faa70422ebe4efaff9ae1004470011f800a54a54
LauAlbert <albertlau123@gmail.com>
2017-09-18 17:18:38 -0700

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '424136003511-rcsivff9h8gvq5jpmbcfrm12f9jtp59p.apps.googleusercontent.com',
+    googleClientSecret: 'Eq2pyqCaLqavbAvQdALK_C-C',
+    mongoURI: 'mongodb://albert:password@ds135624.mlab.com:35624/testtest123',
+    cookieKey: ['dfewrg43trgsbnt','ewf32rhgfxg344']
+};
